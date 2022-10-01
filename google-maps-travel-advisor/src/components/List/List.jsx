bb10c843892fcfd5956cc25b2d600ee0b1af390e
Paulo Roberto Abrahão de Souza <ab.cod@outlook.com>
2022-09-30 22:33:26 -0300

    desenvolvimento do projeto travel advisor

---
+++ google-maps-travel-advisor/src/components/List/List.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
+
+import useStyles from "./styles";
+
+const List = () => {
+  const classes = useStyles();
+
+  const [type, setType] = React.useState("restaurante");
+  const [rating, setRating] = React.useState(0);
+
+  const places = [
+    { name: "1" },
+    { name: "2" },
+    { name: "3" },
+    { name: "1" },
+    { name: "2" },
+    { name: "3" },
+  ];
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h5">
+        Restaurantes, Hoteis e atrações perto de você
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Tipo</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurantes">Restaurantes</MenuItem>
+          <MenuItem value="hoteis">Hoteis</MenuItem>
+          <MenuItem value="atracoes">Atrações</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel>Avaliações</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>Todos</MenuItem>
+          <MenuItem value={3}>Acima de 3</MenuItem>
+          <MenuItem value={4}>Acima de 4</MenuItem>
+          <MenuItem value={4.5}>Acima de 4.5</MenuItem>
+        </Select>
+      </FormControl>
+
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, index) => (
+          <Grid item key={index} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default List;
